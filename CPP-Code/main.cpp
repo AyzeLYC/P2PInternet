83d9fc570f2c76109fc065f84b3b6fa881a89f10
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2026-02-16 21:21:50 +0100

    Updated main.cpp

--- CPP-Code/main.cpp
+++ CPP-Code/main.cpp
@@ -1,1 +1,10 @@
+#include <stdint.h>
 
+#define UINT8 uint8_t
+#define UINT16 uint16_t
+#define UINT32 uint32_t
+
+#include "./application_end_point.cpp"
+#include "./audio.cpp"
+#include "./graphical_user_interface.cpp"
+#include "./input.cpp"
